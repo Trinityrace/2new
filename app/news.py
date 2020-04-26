abd3a112137fc3aaaa451c7a561649d659d99522
Trinity John <trinityrtime@gmail.com>
2020-04-26 16:39:22 +0300

    dynamic routing for articles introuced

--- app/news.py
+++ app/news.py
@@ -8,5 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'World news today'
+    message = 'World news today!'
     return render_template('index.html',message = message)
+
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+
+    '''
+    View movie page function that returns the article details page and its data
+    '''
+    return render_template('article.html',id = article_id)
