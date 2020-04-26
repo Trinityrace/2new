edddd2cfa7a6c7941dc7764323ff1356fe622192
Trinity John <trinityrtime@gmail.com>
2020-04-26 17:15:43 +0300

    configurations for app; and control flows in the index html file

--- app/config.py
+++ app/config.py
@@ -3,3 +3,31 @@
 # cdd57f0bbd8840fc9ccf8c6718290746 
 
 # | 
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
