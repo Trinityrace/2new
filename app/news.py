191c8c70bb730fe0ad359e23508a9c8374c49e31
Trinity John <trinityrtime@gmail.com>
2020-04-26 16:12:34 +0300

    jinja variables introduced to index.html file

--- app/news.py
+++ app/news.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from app import app
 
-# Views
+# News
 @app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'World news today'
+    return render_template('index.html',message = message)
