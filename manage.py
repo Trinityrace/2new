96fc381c1ab328d0854e0af71f3bf9a1537e52bf
Trinity John <trinityrtime@gmail.com>
2020-04-28 01:28:41 +0300

    run.py rename to manage.py, and functions and imports update to run application

---
+++ manage.py
@@ -0,0 +1,27 @@
+from app import create_app
+from flask_script import Manager, Server
+#from app import app
+
+# create app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+#unittest in manager instance
+@manager.command
+def test():
+    '''
+    Runs the unittest
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+#manager.add_command('server', Server)
+if __name__ == '__main__':
+    # app.run(debug = True)
+    #app.run()
+    manager.run()
+
