acc5e5cd0e4da1397e95f0904fed12992c7d7bb3
Trinity John <trinityrtime@gmail.com>
2020-04-28 01:06:58 +0300

    init file for app updated with create_app function and configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,32 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
 
-from app import views
+# from app import views
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    # Registering the BluePrint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting up config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
