915e4b0663fe8beca5b816bb9ee244d5ed9e2bdc
Trinity John <trinityrtime@gmail.com>
2020-04-28 09:42:25 +0300

    pip requirements freeze and stored in requirements

--- config.py
+++ config.py
@@ -1,30 +1,30 @@
-# | 
+# # | 
 
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL=('http://newsapi.org/v2/top-headlines?''apiKey=cdd57f0bbd8840fc9ccf8c6718290746')
-    # pass
-
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     NEWS_API_BASE_URL=('http://newsapi.org/v2/top-headlines?''apiKey=cdd57f0bbd8840fc9ccf8c6718290746')
+#     # pass
 
 
-class DevConfig(Config):
-    '''
-    Development  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
 
-    DEBUG = True
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     pass
+
+
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
+
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+
+#     DEBUG = True
